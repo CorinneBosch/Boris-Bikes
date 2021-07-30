42a00c752d7e6d629aa269d5f23440364845c373
Corinne <corinne.boesch@gmail.com>
2021-07-30 15:26:21 +0100

    updating capacety to 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,35 +1,25 @@
 class DockingStation
 
-  attr_accessor :bikes
+  attr_accessor :station
 
   def initialize
-    @bikes = [] 
+    @station = [] 
   end
 
   def release_bike
-    # (@bikes.empty?) ? (raise 'No bikes available to release') : (@bikes)
-    (dock_empty?) ? (raise 'No bikes available to release') : (@bikes)
+    (dock_empty?) ? (raise 'No bikes available to release') : (@station)
   end
 
   def dock(velo) # takes an instance of a Bike object
-    # (@bikes.count == 1) ? (raise 'Docking station reached full capacety') : (@bikes << velo)
-    (dock_full?) ? (raise 'Docking station reached full capacety') : (@bikes << velo)
-
-    # if dock_full?
-    #   raise 'Docking station reached full capacety'
-    # else @bikes << velo
-    # end
-
-    # @bikes << velo
+  
+    (dock_full?) ? (raise 'Docking station reached full capacety') : (@station << velo)
   end 
 
   def dock_empty?
-    @bikes.empty?
+    @station.empty?
   end
 
   def dock_full?
-    @bikes.count > 1
-    # @bikes.max(1)
+    @station.count >= 20
   end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -26,23 +26,19 @@ describe DockingStation do
   end
 
   it 'raise error when docking_station is empty' do
-    # @bike = []
-    @docking_station.bikes = []
+    @docking_station.station = []
     expect {@docking_station.release_bike}.to raise_error 'No bikes available to release'
   end
 
   it 'raise error when docking_station is full' do
-  
-    @docking_station.bikes = [Bike.new, Bike.new]
-    # @docking_station.dock Bike.new
-    # expect {@docking_station.dock_full?}.to raise_error 'Docking station reached full capacety'
-    # expect(@docking_station.dock_full?).to eq false 
+    20.times { @docking_station.dock Bike.new }
     expect {@docking_station.dock(@bike)}.to raise_error 'Docking station reached full capacety'
   end
 
-  # docking station maintainer
-  # distributing bikes
-  # not accept bikes when full
+  # given
+  # when
+  # then
+
 
 end
 
